980b4421eca52696c223a8983748af755c1888ea
Lin <944799613@qq.com>
2022-03-22 20:52:30 -0500

    Initla Commit

---
+++ InheritanceRevisted/InheritanceRevisited/Models/Media.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InheritanceRevisited.Models
+{
+    public interface iMedia
+    {
+        public string Title { get; set; }
+        public int RunTime { get; set; }
+        public void Play();
+    }
+
+    public class Song : iMedia
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int RunTime { get; set; }
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+        public List<Customer> Customers { get; set; }
+        public void Play()
+        {
+            Console.WriteLine("Playing the song " + Title + " on your favourate music platform for " + RunTime);
+        }
+    }
+
+    public class Video : iMedia
+    {
+        public string Title { get; set; }
+        public int RunTime { get; set; }
+
+        public void Play()
+        {
+            Console.WriteLine("Watching " + Title + " on your inadequate viewing screen for " + RunTime);
+        }
+    }
+}
---
+++ InheritanceRevisted/InheritanceRevisited/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InheritanceRevisited.Models
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<iMedia> Collection { get; set; }
+        public string Email { get; set; }
+        public virtual void GenerateEmail()
+        {
+            this.Email = this.Id.ToString() + Name + "@gmail.com";
+        }
+        public void Binge()
+        {
+            Collection.ForEach(media => media.Play());
+        }
+    }
+
+    public class Artist : User
+    {
+        public List<Song> Songs { get; set; }
+        public override void GenerateEmail()
+        {
+            this.Email = "A" + this.Id.ToString() + Name + "@gmail.com";
+        }
+    }
+
+    public class Customer : User
+    {
+        public List<Song> PurchasedSongs { get; set; }
+    }
+}
